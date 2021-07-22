73192e135cd5dee7f064649771d3c2f7fb5cdf19
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-22 14:59:37 +0800

    fix code

--- src/main/java/com/example/zk/testmain/WatcherClientTest.java
+++ src/main/java/com/example/zk/testmain/WatcherClientTest.java
@@ -21,7 +21,7 @@ public class WatcherClientTest {
 
     private static String CONNECT_STR="192.168.10.33:2185";
 
-    private final static String NAME_SPACE = "watch-curator";
+    private final static String NAME_SPACE = "myRegistry";
 
     private static CuratorFramework curatorFramework;
 
@@ -29,6 +29,7 @@ public class WatcherClientTest {
          curatorFramework = CuratorFrameworkFactory.builder().
                 connectString(CONNECT_STR)
                 .sessionTimeoutMs(5000)
+                 //初次间隔1秒，重试3次。
                 .retryPolicy(new ExponentialBackoffRetry(1000,3))
                 .namespace(NAME_SPACE)
                 .build();
@@ -89,14 +90,16 @@ public class WatcherClientTest {
     }
 
     //=======================================================
-    //1、NodeCache       针对当前节点的创建、删除和更新触发事件
-    //2、PathChildCache  针对于子节点的创建、删除和更新触发事件
-    //3、TreeCache       综合事件(PathChildCache+NodeCache)
+    //1、NodeCache       监听当前节点的创建、删除和更新触发事件
+    //2、PathChildCache  监听子节点的创建、删除和更新触发事件
+    //3、TreeCache       监听该路径下所有节点
     //=======================================================
 
     /**
      * NodeCache
      * 当前节点监听
+     * 1、不会默认创建数据目录。
+     * 2、每次重新启动不会加载历史事件。
      */
     @Test
     public void nodeCache() throws Exception {
@@ -115,19 +118,53 @@ public class WatcherClientTest {
     /**
      * PathChildCache
      * 子节点监听
+     * 1、会默认创建当前节点。
+     * 2、每次重新启动都会监听/watch下每个子目录最后一次的事件。
      */
     @Test
     public void pathChildCache() throws Exception {
+        //false不缓存目录的值。 true缓存目录的值。
         PathChildrenCache nodeCache=new PathChildrenCache(curatorFramework,"/watch",true);
         PathChildrenCacheListener nodeCacheListener = (curatorFramework1,pathChildrenCacheEvent) -> {
-            //CONNECTION_RECONNECTED
-            //CHILD_REMOVED
-            //CHILD_UPDATED
-            //CHILD_ADDED
-            System.out.println(pathChildrenCacheEvent.getType()+"->"+new String(pathChildrenCacheEvent.getData().getData()));
+        //System.out.println("子节点事件类型：" + pathChildrenCacheEvent.getType());
+        //System.out.println("子节点的值：" + new String(pathChildrenCacheEvent.getData().getData()));
+        switch (pathChildrenCacheEvent.getType()) {
+                case CHILD_ADDED:
+                    System.out.println("添加目录 :" + pathChildrenCacheEvent.getData().getPath());
+                    System.out.println("添加数据 : " + new String(pathChildrenCacheEvent.getData().getData()));
+                    break;
+                case CHILD_UPDATED:
+                    System.out.println("更新目录 :" + pathChildrenCacheEvent.getData().getPath());
+                    System.out.println("更新数据 : " + new String(pathChildrenCacheEvent.getData().getData()));
+                    break;
+                case CHILD_REMOVED:
+                    System.out.println("移除目录 :" + pathChildrenCacheEvent.getData().getPath());
+                    System.out.println("移除数据 : " + new String(pathChildrenCacheEvent.getData().getData()));
+                    break;
+                case CONNECTION_RECONNECTED:
+                    System.out.println("======CONNECTION_RECONNECTED======");
+                    break;
+                case CONNECTION_SUSPENDED:
+                    System.out.println("======CONNECTION_SUSPENDED======");
+                    break;
+                case INITIALIZED:
+                    System.out.println("======本地数据缓存成功======");
+                    break;
+                case CONNECTION_LOST:
+                    System.out.println("======CONNECTION_LOST======");
+                    break;
+                default:
+                    System.out.println("======其他类型操作======");
+                    break;
+            }
         };
         nodeCache.getListenable().addListener(nodeCacheListener);
-        nodeCache.start(PathChildrenCache.StartMode.NORMAL);
+        //BUILD_INITIAL_CACHE // 同步初始化客户端的 cache，及创建 cache 后，就从服务器端拉入对应的数据
+        //NORMAL // 异步初始化 cache
+        //POST_INITIALIZED_EVENT // (表示本地数据已缓存成功了)异步初始化，初始化完成触发事件。如果要启用监听，必须使用此参数。
+        //nodeCache.start(PathChildrenCache.StartMode.NORMAL);
+        nodeCache.start(PathChildrenCache.StartMode.NORMAL);//这个模式上面才会触发case INITIALIZED逻辑
+        //nodeCache.start(PathChildrenCache.StartMode.BUILD_INITIAL_CACHE);
         System.in.read();
     }
 
