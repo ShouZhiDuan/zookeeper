44edbbb5071eaafd3680b42c4196a9dc6c89151b
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-22 17:26:52 +0800

    fix code

--- src/main/java/com/example/zk/ZookeeperApplication.java
+++ src/main/java/com/example/zk/ZookeeperApplication.java
@@ -1,13 +1,18 @@
 package com.example.zk;
 
+import com.example.zk.config.BootstrapConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
+/**
+ * 启动类
+ */
 public class ZookeeperApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ZookeeperApplication.class, args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BootstrapConfiguration.class);
+        context.start();
     }
-
 }
---
+++ src/main/java/com/example/zk/config/BootstrapConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.zk.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Description:
+ */
+@Configuration
+@ComponentScan(basePackages={"com.example.zk"})
+public class BootstrapConfiguration {}
